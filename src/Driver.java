250c8967bfc4f302d3ea220f35e2d85790ad5427
mikepmerritt <mikepmerritt@gmail.com>
2021-04-05 00:23:07 -0400

    added first time setup window

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import github.tools.client.GitHubApiClient;
+import github.tools.responseObjects.*;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		new SetupWindow();
+		MainWindow mainWindow = new MainWindow();
+		SetupWindow setupWindow = new SetupWindow();
+		GitHubApiClient gitHubApiClient = connectUser(setupWindow);
+	}
+	
+	// used to locate the credential file in the project
+	private static File findCredentialFile() {
+		File currentDirectory = new File(".");
+		File credentialFile;
+		if (currentDirectory.getAbsolutePath().contains("\\src") || currentDirectory.getAbsolutePath().contains("/src")) {
+			credentialFile = new File("../token.txt");
+		}
+		else {
+			credentialFile = new File("token.txt");
+		}
+		return credentialFile;
 	}
 
+	private static GitHubApiClient connectUser(SetupWindow setupWindow) {
+		GitHubApiClient gitHubApiClient = null;
+		try {
+			File credentialFile = findCredentialFile();
+			Scanner fileScan = new Scanner(credentialFile);
+			String user = fileScan.nextLine();
+			String token = fileScan.nextLine();
+			gitHubApiClient = new GitHubApiClient(user, token);
+			fileScan.close();
+		}
+		catch (FileNotFoundException e) {
+			setupWindow.setVisibility(true);
+		}
+		return gitHubApiClient;
+	}
 }
--- src/SetupWindow.java
+++ src/SetupWindow.java
@@ -5,8 +5,12 @@ public class SetupWindow extends JFrame {
 	public SetupWindow() {
 		super("First Time Setup");
 		this.setSize(400, 250);
-		this.add(new SetupPanel());
-		this.setVisible(true);
+		this.add(new SetupPanel(this));
+		this.setVisible(false);
+	}
+	
+	public void setVisibility(boolean visibility) {
+		this.setVisible(visibility);
 	}
 	
 }
