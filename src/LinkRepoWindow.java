107c4b45c35731a8fdd30fc43b1caa752117be05
mikepmerritt <mikepmerritt@gmail.com>
2021-04-09 19:55:56 -0400

    minor changes to link window and gitignore

--- src/LinkRepoWindow.java
+++ src/LinkRepoWindow.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class LinkRepoWindow extends JFrame{
 
 	public LinkRepoWindow(MainWindow mainWindow) {
-		super("First Time Setup");
+		super("Link a Repo");
 		this.setSize(400, 250);
-		this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(javax.swing.JFrame.HIDE_ON_CLOSE);
 		this.add(new LinkRepoPanel(this, mainWindow));
 	}
 	public void setVisibility(boolean visibility) {
